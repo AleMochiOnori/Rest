2822367643f5cf78c57b0f58a783f21edbcdd544
AlessandroMochiOnori <alessandromochionori@gmail.com>
2024-10-04 19:24:21 +0200

    ciao

--- client.py
+++ client.py
@@ -1,7 +1,36 @@
-import json,requests
+import json
+import requests
+
 
 
 base_url = "http://127.0.0.1:8080"
+
+
+def effettuaPrimoLogin():
+    
+    global username , password , privilegio , primoLoginEffetuato
+    username = input("inserisci username")
+    password = input("inserisci la password")
+    jRequest = {"username " : username , "password" : password}
+    try:
+        api_url = base_url + "/login"
+        response = requests.post(api_url,json=jRequest)
+        if response.status_code == 200 :
+            jResponse = response.json
+            if jResponse["ERROR"] == "000":
+                privilegio : str = jResponse["Privilegio"]
+                primoLoginEffetuato = 1 
+                
+    except:
+        print("Attenzione problemi con il server")
+
+username: str = ""
+password : str = ""
+primoLoginEffetuato = 0
+privilegio : str = ""
+while primoLoginEffetuato == 0 :
+    primoLoginEffetuato = effettuaPrimoLogin()
+
 
 def creaInterfaccia():
     print("Operazioni disponibili : ")
@@ -10,6 +39,11 @@ def creaInterfaccia():
     print("3. Modifica dati cittadino : ")
     print("4. Elimina cittadino : ")
     print("5. Exit")
+
+
+
+
+
 
 
 def richiediDatiCittadino():
@@ -28,13 +62,36 @@ def richiediCodiceFiscale():
     return jRequest
 
 
-
 def modificaCittadino():
-    richiesta = input("Seleziona cosa vuoi modificare : Nome , Cognome , Data di Nascita")
-    
+
+    richiestaModifica = input("Seleziona cosa vuoi modificare : Nome , Cognome , Data di Nascita (Scrivere solo Data) :  ")
+    split = richiestaModifica.split()
+    for parola in split:
+        if parola == "Nome" :
+            richiestaNome = input("inserire il nome modificato : ")
+            return {"campo": "nome", "valore": richiestaNome}
+        if parola == "Cognome" :
+            richiestaCognome = input("inserire il Cognonome modificato : ")
+            return {"campo": "cognome", "valore": richiestaCognome}
+        if parola == "Data":
+            richiestaData = input("inserire la data modicata : ")
+            return {"campo": "data di nascita", "valore": richiestaData}
     
 
 
+def eliminaCittadino():
+    richiestaModifica = input("Seleziona cosa vuoi eliminare : Nome , Cognome , Data di Nascita (Scrivere solo Data) :  ")
+    split = richiestaModifica.split()
+    for parola in split:
+        if parola == "Nome" :
+            richiestaNome = input("inserire il nome da eliminare : ")
+            return {"campo": "nome", "valore": richiestaNome}
+        if parola == "Cognome" :
+            richiestaCognome = input("inserire il Cognonome da eliminare : ")
+            return {"campo": "cognome", "valore": richiestaCognome}
+        if parola == "Data":
+            richiestaData = input("inserire la data da eliminare: ")
+            return {"campo": "data di nascita", "valore": richiestaData}
 
 
 sOper = input("Seleziona operazione da 1 a 5 : ")
@@ -42,7 +99,7 @@ while (sOper != "5"):
     if sOper == "1":
         api_url = base_url + "/add_cittadino"
         jsonDataRequest = richiediDatiCittadino()
-        try:
+        try:    
             response = requests.post(api_url,json=jsonDataRequest)
             print(response.status_code)
             print(response.headers["Content-Type"])
@@ -72,8 +129,71 @@ while (sOper != "5"):
 
 
     if sOper == "3":
-        api_url = base_url + "/richiedi_dati_cittadino"
+        api_url = base_url + "/modifica_cittadino"
         jsonDataRequest = richiediCodiceFiscale()
-        try:
+        try :
+            response = requests.post(api_url,json=jsonDataRequest)
+            if response.status_code == 200 :
+                data1 = response.json()
+                print(response.status_code)
+                print(response.headers["Content-Type"])
+                if data1["ERROR"] == "000":
+                    print("Il codice fiscale esiste")
+                    richiestaModifica = modificaCittadino()
+                    if richiestaModifica :
+                        jsonDataRequest.update(richiestaModifica)
+                        response = requests.post(api_url,json=jsonDataRequest)
+                        print(response.status_code)
+                        print(response.headers["Content-Type"])
+                        data1 = response.json()
+                        print(data1)
+                    else :
+                        print("inserire prima i campi") 
+                else :
+                    print("Il codice fiscale non esiste")
+
+            else:
+                print("il server non ha risposto")
+                            
+        except:
+            print("problemi di comunicazione con il server , riprovare più tardi")
+
+
+
+
+    if sOper == "4":
+        api_url = base_url + "/rimuovi_cittadino"
+        jsonDataRequest = richiediCodiceFiscale()
+        try :
+            response = requests.post(api_url,json=jsonDataRequest)
+            if response.status_code == 200 :
+                data1 = response.json()
+                print(response.status_code)
+                print(response.headers["Content-Type"])
+                if data1["ERROR"] == "000":
+                    print("Il codice fiscale esiste")
+                    richiestaEliminazione = eliminaCittadino()
+                    if richiestaEliminazione :
+                        jsonDataRequest.update(richiestaEliminazione)
+                        response = requests.post(api_url,json=jsonDataRequest)
+                        print(response.status_code)
+                        print(response.headers["Content-Type"])
+                        data1 = response.json()
+                        print(data1)
+                    else :
+                        print("inserire prima i campi") 
+                else :
+                    print("Il codice fiscale non esiste")
+
+            else:
+                print("il server non ha risposto")
+                            
+        except:
+            print("problemi di comunicazione con il server , riprovare più tardi") 
+
+
+
+
+
 
 
--- server.py
+++ server.py
@@ -1,13 +1,21 @@
-from flask import Flask ,json ,request
+from flask import Flask , request
+import json
 from myJason import  JsonSerialize , JsonDeserialize
+import jsonify
 
 
 fileAnagrafe = "./anagrafe.json"
+file_utenti = "./utenti.json"
 api = Flask(__name__)
+
+
+
 
 
 @api.route("/add_cittadino" , methods=["POST"])
 def gestisciCittadino():
+    utenti : dict = JsonDeserialize(file_utenti)
+    if 
     content_type = request.headers.get('Content-Type')
     if content_type == 'application/json':
         jRequest = request.json
@@ -25,6 +33,8 @@ def gestisciCittadino():
     else :
         return "Errore formato non riconosciuti", 401
     
+
+
 
 
 @api.route("/richiedi_dati_cittadino", methods=["POST"])
@@ -49,7 +59,7 @@ def richiediDatiCittadino():
 
 
 
-@api.route("/modifica_cittadino", methods=["POST"])
+@api.route("/modifica_cittadino", methods=["PUT"])
 def modificaCittadino():
     content_type = request.headers.get('Content-Type')
     if content_type == 'application/json' :
@@ -57,24 +67,66 @@ def modificaCittadino():
        print(jRequest)
        sCodiceFiscale = jRequest["Codice Fiscale"]
        dAnagrafe : dict = JsonDeserialize(fileAnagrafe)
-
-    
-
-
-
-
-
-
+       if sCodiceFiscale in dAnagrafe:
+            campoDaModificare = jRequest.get("campo")
+            nuovoValore = jRequest.get("valore")
+            dAnagrafe[sCodiceFiscale][campoDaModificare] = nuovoValore
+            JsonSerialize(dAnagrafe, fileAnagrafe)
+            jResponse = {"ERROR": "000", "Msg": "Modifica effettuata correttamente"}
+            return json.dumps(jResponse), 200       
+       else:
+            jResponse = {"ERROR": "001", "Msg": "Codice fiscale non presente in anagrafe"}
+            return json.dumps(jResponse), 200
+    else:
+        return "Errore: formato non riconosciuto", 401 
            
-
            
+@api.route("/rimuovi_cittadino", methods=["DELETE"])
+def rimuoviCittadino():
+    content_type = request.headers.get('Content-Type')
+    if content_type == 'application/json' :
+       jRequest = request.json
+       print(jRequest)
+       sCodiceFiscale = jRequest["Codice Fiscale"]
+       dAnagrafe : dict = JsonDeserialize(fileAnagrafe)
+       if sCodiceFiscale in dAnagrafe:
+            campoDaEliminare = jRequest.get("campo")
+            dAnagrafe[sCodiceFiscale].pop(campoDaEliminare)
+            JsonSerialize(dAnagrafe, fileAnagrafe)
+            jResponse = {"ERROR": "000", "Msg": "Modifica effettuata correttamente"}
+            return json.dumps(jResponse), 200
+       else:
+            jResponse = {"ERROR": "001", "Msg": "Codice fiscale non presente in anagrafe"}
+            return json.dumps(jResponse), 200
+    else:
+        return "Errore: formato non riconosciuto", 401 
+           
+file_path_users = "utenti.json"
+utenti = JsonDeserialize(file_path_users)
 
 
 
+@api.route("/login" , methods = ["POST"])
+def primoLogin():
 
-
-
-
+    content_type = request.headers.get('Content-Type')
+    if content_type == 'application/json' :
+       jRequest : dict = request.json
+       # Jrequest =  {username : mario  , "password" : "cikolino123"}
+       print(jRequest)
+       usernameInseritoDaClient = jRequest["username"]
+       if usernameInseritoDaClient in utenti :
+           passwordInseritaDalClient = jRequest["password"]
+           if passwordInseritaDalClient == utenti[usernameInseritoDaClient]["password"]:
+               privilegio = utenti[usernameInseritoDaClient]["privilegi"]
+               return jsonify({"Esito" : "000" ,"MSG" : "Utente registrato" , "Privilegio" : privilegio})
+           else : 
+               return jsonify({"Esito" : "000" ,"MSG" : "Credenziali errate"})
+           
+       else : 
+           return jsonify({"Esito" : "000" ,"MSG" : "Credenziali errate"})
+    else :
+        return jsonify({"Esito" : "000" ,"MSG" : "Formato dati errato"})
 
 if __name__ == '__main__':
     api.run(host="127.0.0.1", port=8080)
